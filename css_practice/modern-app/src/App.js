4d27292ef46b5ddba123714714212539b745d10f
MalikVedanshu <malikvedanshu757@outlook.com>
2022-12-07 10:23:20 +0000

    learning css

---
+++ css_practice/modern-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1>Thank you for getting in the course</h1>
+      <div className='container'>
+      <div className="box1">This is a box</div>
+      </div>
+      </>
+  );
+}
+
+export default App;
