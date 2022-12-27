9c983a3b7cf7d156da861acf0895a1145490fb45
MalikVedanshu <malikvedanshu757@outlook.com>
2022-12-27 20:56:20 +0000

    setup bootrap done

--- css_practice/Bootstrap/advance/src/App.js
+++ css_practice/Bootstrap/advance/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Hello hey</h1>
+    <button type="button" className="btn btn-danger">Danger</button>
+    <button type="button" className="btn btn-warning">Warning</button><br />
+    </>
+  )
 }
 
 export default App;
