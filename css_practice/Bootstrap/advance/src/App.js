9f2140fd8c2423bcd4334460fd3eb9b988c26b74
MalikVedanshu <malikvedanshu757@outlook.com>
2022-12-28 19:38:07 +0000

    advance bootstrap

--- css_practice/Bootstrap/advance/src/App.js
+++ css_practice/Bootstrap/advance/src/App.js
@@ -6,6 +6,20 @@ function App() {
     <h1>Hello hey</h1>
     <button type="button" className="btn btn-danger">Danger</button>
     <button type="button" className="btn btn-warning">Warning</button><br />
+
+    <div className="container text-center">
+    <div class="row">
+    <div class="col">
+      <input className='btn btn-primary' value="Column1" />
+    </div>
+    <div class="col">
+      <input className='btn btn-secondary' value="Column2" />
+    </div>
+    <div class="col">
+      <input className='btn btn-info' value="Column3" />
+    </div>
+    </div>
+    </div>
     </>
   )
 }
