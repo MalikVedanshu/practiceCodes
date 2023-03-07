c156217e4d75f8a5da543856a32af0f4e9111375
MalikVedanshu <malikvedanshu757@outlook.com>
2023-03-07 18:14:41 +0000

    Link list

---
+++ dsa/LinkList/mergeSortedLinkLists.js
@@ -0,0 +1,36 @@
+/* 
+Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+*/
+
+/*
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+
+var mergeTwoLists = function(list1, list2) {
+  
+};
+
+let list1 = {"value":{"value":{"value":{"value":{"value":{"value":null,"next":10},"next":20},"next":30},"next":50},"next":60}}
+
